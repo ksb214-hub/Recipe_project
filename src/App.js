8ba00a384948638541d16b6051c9a55eff8bcf14
ksb214-hub <77158885+ksb214-hub@users.noreply.github.com>
2026-03-19 13:03:19 +0900

    ㅎ

--- src/App.js
+++ src/App.js
@@ -1,11 +1,32 @@
 import "./App.css";
 import Login from "./Login";
+import SearchPage from "./SearchPage";
+
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 
 function App() {
   return (
-    <div className="app">
-      <Login />
-    </div>
+    <Router>
+      <div className="app">
+
+        {/*  네비바 */}
+        <nav className="navbar">
+          <h2 className="logo">🍳 Recipe</h2>
+
+          <div className="nav-buttons">
+            <Link to="/search" className="nav-btn">검색</Link>
+            <Link to="/" className="nav-btn">로그인</Link>
+          </div>
+        </nav>
+
+        {/*  페이지 영역 */}
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/search" element={<SearchPage />} />
+        </Routes>
+
+      </div>
+    </Router>
   );
 }
 
